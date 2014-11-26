d2e16d01f376712a08e544e79109b50588f14374
wuqihui <wu_qihui@163.com>
2014-11-26 11:35:44 +0800

    webUploader的demo

---
+++ Demos/MvcApplication/Controllers/WebuploaderController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication.Controllers
+{
+    public class WebuploaderController : Controller
+    {
+        //
+        // GET: /Webuploader/
+
+        public ContentResult UploadPic(HttpPostedFileBase file)
+        {
+            return new ContentResult();
+        }
+
+        public ActionResult Demo()
+        {
+            return View();
+        }
+    }
+}
