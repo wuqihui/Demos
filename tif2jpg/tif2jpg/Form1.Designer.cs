d566840f1fb7690a3b4000ee861da1cea14ec182
wuqihui <wu_qihui@163.com>
2015-03-04 15:04:50 +0800

    Tif 转jpg

---
+++ tif2jpg/tif2jpg/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace tif2jpg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrower = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnTif2jpg = new System.Windows.Forms.Button();
+            this.labelPath = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnBrower
+            // 
+            this.btnBrower.Location = new System.Drawing.Point(443, 43);
+            this.btnBrower.Name = "btnBrower";
+            this.btnBrower.Size = new System.Drawing.Size(75, 23);
+            this.btnBrower.TabIndex = 0;
+            this.btnBrower.Text = "浏览";
+            this.btnBrower.UseVisualStyleBackColor = true;
+            this.btnBrower.Click += new System.EventHandler(this.btnBrower_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(52, 45);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(385, 20);
+            this.txtPath.TabIndex = 1;
+            // 
+            // btnTif2jpg
+            // 
+            this.btnTif2jpg.Location = new System.Drawing.Point(524, 43);
+            this.btnTif2jpg.Name = "btnTif2jpg";
+            this.btnTif2jpg.Size = new System.Drawing.Size(75, 23);
+            this.btnTif2jpg.TabIndex = 2;
+            this.btnTif2jpg.Text = "转换";
+            this.btnTif2jpg.UseVisualStyleBackColor = true;
+            this.btnTif2jpg.Click += new System.EventHandler(this.btnTif2jpg_Click);
+            // 
+            // labelPath
+            // 
+            this.labelPath.AutoSize = true;
+            this.labelPath.Location = new System.Drawing.Point(12, 48);
+            this.labelPath.Name = "labelPath";
+            this.labelPath.Size = new System.Drawing.Size(37, 13);
+            this.labelPath.TabIndex = 3;
+            this.labelPath.Text = "路径：";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(52, 100);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(547, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "进度:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 168);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.labelPath);
+            this.Controls.Add(this.btnTif2jpg);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnBrower);
+            this.Name = "Form1";
+            this.Text = "Tif2Jpg---Keith.wu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrower;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnTif2jpg;
+        private System.Windows.Forms.Label labelPath;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ tif2jpg/tif2jpg/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tif2jpg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string _tifPath;
+        private string _tifName;
+        private void btnTif2jpg_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(_tifPath))
+            {
+                if (_tifPath.LastIndexOf(".tif") < 0)
+                {
+                    MessageBox.Show(@"Selected file is incorret");
+                }
+                else
+                {
+                    tif2jpg();
+                    MessageBox.Show(@"Success.");
+                }
+            }
+            else
+            {
+                MessageBox.Show(@"please slecte file");
+            }
+        }
+
+        private void btnBrower_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            _tifPath = openFileDialog1.FileName; 
+            _tifName = Path.GetFileName(openFileDialog1.FileName);
+            this.txtPath.Text = _tifPath;
+        }
+
+        public void tif2jpg()
+        {  
+                int len =_tifPath.LastIndexOf(".tif");
+
+                _tifName = _tifName.Substring(0, _tifName.Length - 4);  
+                string filePath = Path.GetDirectoryName(_tifPath);
+
+                FileStream stream = File.OpenRead(_tifPath);
+                Bitmap bmp = new Bitmap(stream);
+                Image image = bmp;
+                Guid objGuid = image.FrameDimensionsList[0];
+                FrameDimension objDimension = new FrameDimension(objGuid);
+                int totFrame = image.GetFrameCount(objDimension);
+            this.progressBar1.Maximum = totFrame;
+                //  count = totFrame;
+                for (int i = 0; i < totFrame; i++)//循环生成多张图片
+                {
+                    image.SelectActiveFrame(objDimension, i);
+                    string newName =string.Format("{0}_{1}.jpg", _tifName,i+1);
+                    string newPaht = Path.Combine(filePath, newName);
+                    image.Save(newPaht, ImageFormat.Jpeg);
+                    this.progressBar1.Value = i + 1;
+                }
+                bmp.Dispose();
+                image.Dispose();
+                stream.Close();  
+
+        }
+
+    }
+}
